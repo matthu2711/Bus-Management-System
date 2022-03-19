561474fca45698b742c1eb070b48652991bd0e82
Matt <wallacm4@tcd.ie>
2022-03-19 23:37:03 +0000

    - Changed from EdgeWeightedDigraph DS to AdjMatrixEdgeWeightedDigraph DS to make use of FloydWarshall methods

--- main.java
+++ main.java
@@ -1,14 +1,21 @@
-import edu.princeton.cs.algs4.DirectedEdge;
-import edu.princeton.cs.algs4.EdgeWeightedDigraph;
+import edu.princeton.cs.algs4.*;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
 
 public class main {
 
     public static void main(String[] args) throws IOException {
+        AdjMatrixEdgeWeightedDigraph graph = createGraph();
+        FloydWarshall fw = new FloydWarshall(graph);
 
+
+
+    }
+
+    private static AdjMatrixEdgeWeightedDigraph createGraph() throws FileNotFoundException {
         Scanner sr = new Scanner(new File("stops.txt"));
 
         sr.nextLine();
@@ -23,7 +30,7 @@ public class main {
 
         Collections.sort(stops);
 
-        EdgeWeightedDigraph graph = new EdgeWeightedDigraph(stops.get(stops.size() - 1) + 1);
+        AdjMatrixEdgeWeightedDigraph graph = new AdjMatrixEdgeWeightedDigraph(stops.get(stops.size() - 1) + 1);
         sr = new Scanner(new File("transfers.txt"));
         sr.nextLine();
 
@@ -45,20 +52,15 @@ public class main {
             String[] values = sr.nextLine().split(",");
             if(Integer.parseInt(values[0]) != tripID){
                 tripID = Integer.parseInt(values[0]);
-                pastStop = Integer.parseInt(values[3]);
             }
             else {
                 graph.addEdge(new DirectedEdge(pastStop, Integer.parseInt(values[3]), 1));
-                pastStop = Integer.parseInt(values[3]);
             }
+            pastStop = Integer.parseInt(values[3]);
         }
 
         System.out.println(graph.toString());
-
-
-
-
-
+        return graph;
     }
 }
 
